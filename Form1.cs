83bdce029d83c154cae4f6d601d5136caa2cde18
AUTOSYSTEM <autosystemco@gmail.com>
2024-03-15 12:45:42 +0330

    Add hello world message

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace test_git_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world!")
+        }
     }
 }
