f69d2492809c215ac574fc0d7849c154ba42d9c2
autosystem <autosystemco@gmail.com>
2024-03-15 12:54:50 +0330

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,9 @@ namespace test_git_1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e);
+        private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world!")
+            MessageBox.Show("Hello world1!");
         }
     }
 }
